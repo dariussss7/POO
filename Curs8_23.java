77e7a787eb8320d6db75f11a82985bf7ea6302d9
dariussss7 <d_ifrose@yahoo.com>
2024-11-20 21:37:28 +0200

    Add files via upload

---
+++ Curs8_23.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.curs8_23;
+
+/**
+ *
+ * @author d_ifr
+ */
+public class Curs8_23 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
